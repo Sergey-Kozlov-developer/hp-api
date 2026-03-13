1db1335a0f2349d74bcc7893b7565b91bca81e81
Sergei Kozlov <sergeikozlov@MacBook-Air-Sergei.local>
2026-03-13 14:55:01 +0300

    start project

---
+++ src/0_app/App.tsx
@@ -0,0 +1,12 @@
+
+import {css} from "@app/styles/styled-system/css";
+
+function App() {
+
+
+  return (
+    <div className={css({fontSize: '2xl', color: 'red'})}>Hello 🐼!</div>
+  )
+}
+
+export default App
