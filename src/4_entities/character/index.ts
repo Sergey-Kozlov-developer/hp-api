c9898d2a1f8a42c1ce714027a845fc8a0d6072cf
Sergei Kozlov <sergeikozlov@MacBook-Air-Sergei.local>
2026-03-13 16:17:37 +0300

    refactor: add import entities character

---
+++ src/4_entities/character/index.ts
@@ -0,0 +1,3 @@
+export {characterApi} from './model/character-api.ts'
+export type {ICharacter, ICharacterParams, ICharactersResponse} from './model/type.ts'
+export { useGetCharactersQuery, useGetCharacterByIdQuery, useLazyGetCharacterByIdQuery } from './model/character-api.ts';
